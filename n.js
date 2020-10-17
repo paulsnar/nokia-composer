052cabb181f6e12899f393a6f99eef3f8c2885e5
paulsnar <paulsnar@paulsnar.lv>
2020-10-17 22:57:17 +0300

    Drop a bunch of changes
    A short summary:
    
    - Ungolf the code. It's a nice challenge to write it concisely, but I
      prefer a more expanded form.
    - Use ramps for the gain node to make things sound smoother during note
      start/end.
    - Learn to use 0th octave.
    - Learn an alternate sharp/flat syntax -- can use both #d and d#,
      as well as bd and db (in both cases unambiguous).
    - Learn double dot.
    - Stop automatically after finishing the song.
    - Extend note/pause timing to 80/20 instead of 70/30.
    
    In the interim, seems like my pitch generation code is slightly off for
    flats and the like (a typical all-black-key pentatonic song sounds
    really off.)

--- n.js
+++ n.js
@@ -1,26 +1,87 @@
-C = 0; // Initial value for audio context is zero
-stop = _ => C && C.close((C = 0)); // interrupt the playback, if any
-c = (x = 0, a, b) => (x < a && (x = a), x > b ? b : x); // clamping function (a<=x<=b)
-play = (s, bpm) => {
-  // Create audio context, square oscillator and gain to mute/unmute it
-  C = new AudioContext();
-  (z = C.createOscillator())
-    .connect((g = C.createGain()))
-    .connect(C.destination);
-  z.type = 'square';
-  z.start();
-  t = 0; // current time counter, in seconds
-  v = (x, v) => x.setValueAtTime(v, t); // setValueAtTime shorter alias
-  for (m of s.matchAll(/(\d*)?(\.?)(#?)([a-g-])(\d*)/g)) {
-    k = m[4].charCodeAt(); // note ASCII [0x41..0x47] or [0x61..0x67]
-    n = 0 | ((((k & 7) * 1.6 + 8) % 12) + !!m[3] + 12 * c(m[5], 1, 3)); // note index [0..35]
-    v(z.frequency, 261.63 * 2 ** (n / 12));
-    v(g.gain, (~k & 8) / 8);
-    // note duration, measured in 1/10 seconds to simplify further ratios,
-    // i.e. multiply by 7 instead of 0.7
-    d = (24 / bpm / c(m[1] || 4, 1, 64)) * (1 + !!m[2] / 2);
-    t = t + d*7;
-    v(g.gain, 0);
-    t = t + d*3;
+let ctx = null;
+let stopper = null;
+
+window.stop = () => {
+  if (ctx !== null) {
+    ctx.close();
+    ctx = null;
   }
+  if (stopper !== null) {
+    clearTimeout(stopper);
+    stopper = null;
+  }
+};
+
+const clamp = (val, min, max) => val < min ? min : val > max ? max : val;
+
+window.play = (song, bpm) => {
+  ctx = new AudioContext();
+  let buzzer = ctx.createOscillator(),
+      gain = ctx.createGain();
+  buzzer.connect(gain);
+  gain.connect(ctx.destination);
+
+  buzzer.type = 'square';
+  buzzer.start();
+
+  let t = 0;
+  const spb = 60 / bpm;
+  const jiffy = spb / 256;
+
+  const octaves = [220, 440, 880, 1760, 2640],
+        pitchOrder = ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b'],
+        pitches = [2**(-9/12), 2**(-8/12), 2**(-7/12), 2**(-6/12), 2**(-5/12), 2**(-4/12), 2**(-3/12), 2**(-2/12), 2**(-1/12), 1, 2**(1/12), 2**(2/12)];
+
+  for (let note of song.matchAll(/(\d*)?(\.?\.?)([#b]?)([a-h-])([#b]?)(\d?)/gi)) {
+    let [_, length, dots, mod1, noteName, mod2, octave] = note.map(x => x.toLowerCase());
+    noteName = noteName.toLowerCase();
+    if (noteName === 'h') {
+      noteName = 'b';
+    } else if (noteName === '-') {
+      if (t === 0) {
+        gain.gain.setValueAtTime(0.0, 0.0);
+      }
+      t += spb * 4 / Number(length);
+      continue;
+    }
+    let mod = mod2 || mod1;
+    if (mod === '#') {
+      noteName += '#';
+    }
+    let noteIndex = pitchOrder.indexOf(noteName);
+    if (mod === 'b') {
+      noteIndex = (noteIndex + pitches.length - 1) % pitches.length;
+    }
+    if (octave === '') {
+      octave = 1;
+    } else {
+      octave = clamp(Number(octave), 0, octaves.length - 1);
+    }
+    let pitch = pitches[noteIndex] * octaves[Number(octave)];
+
+    let len = 4 / Number(length);
+    if (dots === '.') {
+      len *= 1.5;
+    } else if (dots === '..') {
+      len *= 1.75;
+    }
+    len *= spb;
+
+    if (t !== 0) {
+      buzzer.frequency.setValueAtTime(pitch, t - jiffy);
+      gain.gain.setValueAtTime(0.0, t - jiffy);
+      gain.gain.linearRampToValueAtTime(1.0, t);
+    } else {
+      buzzer.frequency.setValueAtTime(pitch, 0.0);
+      gain.gain.setValueAtTime(1.0, 0.0);
+    }
+    t += 0.8 * len;
+    gain.gain.setValueAtTime(1.0, t);
+    gain.gain.linearRampToValueAtTime(0.0, t + jiffy);
+    t += 0.2 * len;
+  }
+
+  stopper = setTimeout(() => {
+    togglePlayback();
+  }, (t + 3) * 1000);
 };
